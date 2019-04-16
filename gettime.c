d90c28c0a1dd1370d17d3cda4ad466ba6f84c7a5
Tanachot Chirakitsakul <42908486+cotver@users.noreply.github.com>
2019-04-16 17:35:59 +0700

    Update gettime.c

--- gettime.c
+++ gettime.c
@@ -2,31 +2,23 @@
 
 
 void get_time(){
-	// variables to store date and time components
 	int hours, minutes, seconds, day, month, year;
-
 	// time_t is arithmetic time type
 	time_t now;
 	
 	// Obtain current time
-	// time() returns the current time of the system as a time_t value
 	time(&now);
 
-	// Convert to local time format and print to stdout
-	printf("Today is : %s", ctime(&now));
-
-	// localtime converts a time_t value to calendar time and 
 	// returns a pointer to a tm structure with its members 
 	// filled with the corresponding values
 	struct tm *local = localtime(&now);
 
-    hours = local->tm_hour;      	// get hours since midnight (0-23)
-    minutes = local->tm_min;     	// get minutes passed after the hour (0-59)
-    seconds = local->tm_sec;     	// get seconds passed after minute (0-59)
-
-    day = local->tm_mday;        	// get day of month (1 to 31)
-    month = local->tm_mon + 1;   	// get month of year (0 to 11)
-    year = local->tm_year + 1900;	// get year since 1900
+	day = local->tm_mday;        	// get day of month (1 to 31)
+	month = local->tm_mon + 1;   	// get month of year (0 to 11)
+	year = local->tm_year + 1900;	// get year since 1900
+	hours = local->tm_hour;      	// get hours since midnight (0-23)
+	minutes = local->tm_min;     	// get minutes passed after the hour (0-59)
+	seconds = local->tm_sec;     	// get seconds passed after minute (0-59)
 
 	times.day = day;
 	times.month = month;
